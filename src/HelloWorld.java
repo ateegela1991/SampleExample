75264893988cafac8360ec0e78a0cc01c974d2e8
DESKTOP-D5SQ4VF\ashis <aashish.workmail@gmail.com>
2018-09-16 00:06:35 -0500

    Created a method to print name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("HelloWorld");
+		
+		HelloWorld hw = new HelloWorld();
+		hw.name("Ashish Reddy Teegala");
+	}
+	
+	public void name(String name){
+		System.out.println("My Name is " + name);
 	}
 
 }
